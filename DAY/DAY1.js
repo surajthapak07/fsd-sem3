7529dd81fef4769a022cb072c4d3ccb311ddf98d
Suraj Thapak <thapaksuraj394@gmail.com>
2026-07-22 11:12:08 +0530

    day1.js

---
+++ DAY/DAY1.js
@@ -0,0 +1,35 @@
+console.log("Hello, World!");
+
+//variables
+var name="Suraj";
+console.log(name);
+console.log(typeof(name));
+
+
+var name="Suraj";
+
+console.log(name);
+
+//let
+let age=22;
+console.log(age);
+console.log(typeof(age));
+
+
+ 
+let a=10;
+let b="suraj";
+let c=true;
+let d=null;
+let e=undefined;
+console.log(typeof(a));
+console.log(typeof(b));
+console.log(typeof(c));
+console.log(typeof(d));
+console.log(typeof(e));
+let abc ={
+    name:"suraj"
+}
+
+console.log(abc);
+
